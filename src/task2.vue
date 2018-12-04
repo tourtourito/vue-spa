78f5e87db8d11d6f444bb523c5df0d73057c98ad
arthur labeyrie <arthur.labeyrie@efrei.net>
2018-12-04 11:07:04 +0100

    Initial Vue.js project

---
+++ src/task2.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div>
+      <h1>Participants list</h1>
+      <ol>
+        <li>John Doe</li>
+        <li>Robin Hood</li>
+        <li>Chuck Norris</li>
+      </ol>
+    </div>
+
+    <h3>New participant</h3>
+    <form @submit.prevent="addNewParticipant()">
+      <label>Firstname</label>
+      <input type="text">
+      <label>Lastname</label>
+      <input type="text">
+      <button>Add new participant</button>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {};
+    },
+    methods: {
+      addNewParticipant() {
+        // dunno, what's next?
+      }
+    }
+  };
+</script>
